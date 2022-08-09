236c59a4933ce105cd0999c4207c085b5da6d583
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-08-09 15:05:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Signup from "./components/signup";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
