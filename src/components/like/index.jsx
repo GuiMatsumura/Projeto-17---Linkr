d7dbed71a47558c064945886b14c618558a14283
Lucas Duarte <lukazds2016@gmail.com>
2022-08-12 23:20:57 -0300

    connecting like with the API

--- src/components/like/index.jsx
+++ src/components/like/index.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext";
+import axios from "axios";
 
 export default function Like() {
 
     const [clickedLike, setClickedLike] = useState(false);
+
+    const { token } = useContext(UserContext);
+    const defaultToken = token ? token : localStorage.getItem("token");
 
     const COLOR_LIKE = "red";
     const COLOR_NOLIKE = "white";
@@ -11,9 +17,22 @@ export default function Like() {
     function makeLike() {
         return (
             clickedLike ?
-                <AiFillHeart fill={COLOR_LIKE} onClick={() => setClickedLike(!clickedLike)} /> :
-                <AiOutlineHeart fill={COLOR_NOLIKE} onClick={() => setClickedLike(!clickedLike)} />
+                <AiFillHeart fill={COLOR_LIKE} onClick={() => submitLike()} /> :
+                <AiOutlineHeart fill={COLOR_NOLIKE} onClick={() => submitLike()} />
         )
+    }
+
+    function submitLike() {
+
+        const body = { like: clickedLike, postId: "1" }
+        const URL = "http://localhost:4000/likes"
+        const config = { headers: { Authorization: 'Bearer ' + defaultToken } }
+
+        const request = axios.post(URL, body, config);
+
+        request.then(() => { setClickedLike(!clickedLike) })
+
+        request.catch(() => alert("Post Id inexistente!!!"))
     }
 
     return makeLike();
