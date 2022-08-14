f8099c7bb0ef3287011056b0f06ea8add11c42ff
thaisealves <alves.thaisesilva@gmail.com>
2022-08-14 02:53:59 -0300

    feat: getting the filter logic from the input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,62 @@
 import styled from "styled-components";
 import { FaSearch } from "react-icons/fa";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function Search({ display }) {
+  const [searchField, setSearchField] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState([]);
+  async function getAllUsers() {
+    const allUsers = await getUsers();
+    console.log(searchField);
+    setFilteredPersons(
+      allUsers.filter((person) => {
+        return (
+          person.username.toLowerCase().includes(searchField.toLowerCase()) ||
+          person.email.toLowerCase().includes(searchField.toLowerCase())
+        );
+      })
+    );
+  }
+
+  useEffect(() => getAllUsers(), [searchField]);
+
   return (
     <Container display={display}>
-      <input type="text" placeholder="Search for people" />
+      <input
+        type="text"
+        placeholder="Search for people"
+        onChange={(e) => setSearchField(e.target.value)}
+      />
       <FaSearch />
+      <SearchList filteredPersons={filteredPersons} />
     </Container>
+  );
+}
+
+async function getUsers() {
+  try {
+    const response = await axios.get("http://localhost:4000/users");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possivel encontrar os usuários");
+  }
+}
+
+function SearchList({ filteredPersons }) {
+  const filtered = filteredPersons.map((person) => {
+    return <List key={person.userId} person={person} />;
+  });
+  console.log(filtered);
+  return filtered ? <StyledList>{filtered}</StyledList> : null;
+}
+
+function List({ person }) {
+  return (
+    <li>
+      <img src={person.photo} />
+      <h2>{person.username}</h2>
+    </li>
   );
 }
 
@@ -30,5 +80,14 @@ const Container = styled.div`
     &::placeholder {
       color: #c6c6c6;
     }
+  }
+`;
+
+const StyledList = styled.ul`
+  background-color: gray;
+  color: red;
+  img {
+    height: 39px;
+    width: 39px;
   }
 `;
